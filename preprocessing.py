da64ffb6586d4a780f2191aca1de09ca97ff68c1
teostereciu <teostereciu@gmail.com>
2023-05-30 13:01:10 +0200

    getting there

--- preprocessing.py
+++ preprocessing.py
@@ -11,6 +11,14 @@ import pandas as pd
 
 lemmatizer = WordNetLemmatizer()
 
+def delete_numbers(data):
+    for i in range(5):
+        for item in data['source'][i]:
+            print(item)
+        if len(data['source'].loc[i]) < 3:
+            data.drop(index=i)
+    return data
+
 def load_data(source, target):
     data_source = []
     with open(source, "r") as f:
@@ -46,21 +54,35 @@ def get_wordnet_pos(treebank_tag):
         return wordnet.NOUN
 
 # turn a review into clean tokens
-def clean_data(doc):
-    doc = contractions.fix(doc)
+def clean_data(doc, expand, lemma):
+    if expand:
+        doc = contractions.fix(doc)
     doc = re.sub(r'[^a-zA-Z0-9.!?]+', ' ', doc) # remove every char that is not alphanumeric or end of sentence punctuation, keep spaces
     tokens = wordpunct_tokenize(doc) 
     lowercase_tokens = [token.lower() for token in tokens]
-    pos = pos_tag(lowercase_tokens)
-    clean_tokens = [lemmatizer.lemmatize(word, get_wordnet_pos(pos_tag)) for (word, pos_tag) in pos]
-
+    if lemma:
+        pos = pos_tag(lowercase_tokens)
+        clean_tokens = [lemmatizer.lemmatize(word, get_wordnet_pos(pos_tag)) for (word, pos_tag) in pos]
+    else:
+        clean_tokens = lowercase_tokens
     return clean_tokens
 
 def main():
     df = load_data('hu-en/europarl-v7.hu-en.en', 'hu-en/europarl-v7.hu-en.hu')
-    df['source'] = df['source'].apply(clean_data)
+    df['source'] = df['source'].apply(lambda x: clean_data(x, expand=True, lemma=True))
+    df['target'] = df['target'].apply(lambda x: clean_data(x, expand=False, lemma=False))
     df.to_csv('clean_df', index=False)
     print(df.head())
+
+    # Save to pickle
+    df.to_pickle('clean-data.pkl')
+    # Save with compression
+    df.to_pickle('clean-data.pkl.gz', compression='gzip')
+
+    # Load pickle from disk
+    df = pd.read_pickle('clean-data.pkl')
+
+    print(df.head(20))
 
 if __name__ == "__main__":
     main()
