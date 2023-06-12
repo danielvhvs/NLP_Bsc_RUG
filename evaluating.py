9416635ded2e9e1112e04b77943ea6f8dd47bc43
teostereciu <teostereciu@gmail.com>
2023-06-12 10:23:38 +0200

    working on it

--- evaluating.py
+++ evaluating.py
@@ -1,39 +1,52 @@
 from pickle import load
 from keras.models import load_model
 from sklearn.model_selection import train_test_split
-from keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 import pandas as pd
+import numpy as np
+
 
 def splitting():
-    df = pd.read_pickle('../clean-data.pkl')
+    df = pd.read_pickle('../clean-data.pkl')[:10000]
     x_train, x_test, y_train, y_test = train_test_split(df["source"], df["target"],
-                                                        test_size=0.2,random_state=42)
+                                                        test_size=0.2, random_state=42)
     return x_train, x_test, y_train, y_test
 
-def encoding(sequences,tokenizer,length):
+
+def encoding(sequences, tokenizer, length):
     seq = tokenizer.texts_to_sequences(sequences)
-    seq = pad_sequences(seq, maxlen=length,padding="post")
+    seq = pad_sequences(seq, maxlen=length, padding="post")
     return seq
 
-def vector_to_word(embedding,tokenizer):
-    for word,transform in tokenizer.word_index.items():
-        if embedding == transform:
+
+def vector_to_word(embedding, tokenizer):
+    idx = np.argmax(embedding)
+    print("argmax", idx)
+    for word, transform in tokenizer.word_index.items():
+        if transform == idx:
             return word
     return None
 
-def data_to_sentences(sequences,tokenizer):
+
+def data_to_sentences(sequences, tokenizer):
     predictions = []
     for sentence in sequences:
-        predict = []
-        for idx in range(len(sentence)):
-            word = vector_to_word(sentence[idx],tokenizer)
-            if idx > 0:
-                if word != vector_to_word(sentence[idx-1],tokenizer) and word !=None:
-                    predict.append(word)
-            elif word !=None:
-                predict.append(word)
-        predictions.append(" ".join(predict))
+        predict = ''
+        for emb in sentence:
+            word = vector_to_word(emb, tokenizer)
+            if word is not None:
+                predict += word + ' '
+        predictions.append(predict)
+        # for idx in range(len(sentence)):
+        #    word = vector_to_word(sentence[idx], tokenizer)
+        #    if idx > 0:
+        #        if word != vector_to_word(sentence[idx-1], tokenizer) #and word != None:
+        #            predict.append(word)
+        #    elif word != None:
+        #        predict.append(word)
+        # predictions.append(" ".join(predict))
     return predictions
+
 
 def join_every(sequences):
     new = []
@@ -41,25 +54,35 @@ def join_every(sequences):
         new.append(" ".join(i))
     return new
 
+
 def evaluating_proces():
     source, source_test, target, target_test = splitting()
-    en_tokenizer = load(open('en_tokenizer.pkl', 'rb'))
-    hu_tokenizer = load(open('hu_tokenizer.pkl', 'rb'))
-    en_size = len(en_tokenizer.word_index) + 1
-    hu_size = len(hu_tokenizer.word_index) + 1
-    
-    input_test = encoding(source_test,en_tokenizer,len(source[0]))
-    output_test = encoding(target_test,hu_tokenizer,len(source[0]))
+    en_tokenizer = load(open('../en_tokenizer.pkl', 'rb'))
+    hu_tokenizer = load(open('../hu_tokenizer.pkl', 'rb'))
+
+    en_seq_length = len(source.iloc[0])*2
+    hu_seq_length = len(source.iloc[0])*2
+
+    # input_test = encoding(source_test, en_tokenizer, en_seq_length)
+    # output_test = encoding(target_test, hu_tokenizer, hu_seq_length)
+
+    input_train = encoding(source, en_tokenizer, en_seq_length)
+    output_train = encoding(target, hu_tokenizer, hu_seq_length)
 
     model = load_model("../model.h5")
 
-    prediction = model.predict(input_test)
-    
-    prediction_sentences = data_to_sentences(prediction,hu_tokenizer)
-    actual_sentences = join_every(target_test)
-    pred_df = pd.DataFrame({"actual":actual_sentences,"prediction":prediction_sentences})
-    print(pred_df)
+    prediction = model.predict(input_train[0:2])
+
+    print(prediction)
+
+    prediction_sentences = data_to_sentences(prediction, hu_tokenizer)
+    print(prediction_sentences)
+    # actual_sentences = join_every(target_test)
+    # pred_df = pd.DataFrame(
+    #    {"actual": actual_sentences, "prediction": prediction_sentences})
+    # print(pred_df)
     return
+
 
 if __name__ == "__main__":
     evaluating_proces()
--- training.py
+++ training.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import re
 import string
-from keras.layers import LSTM,Embedding,Dense,RepeatVector
+from keras.layers import LSTM, Embedding, Dense, RepeatVector
 from keras.models import Sequential
 from keras.preprocessing.text import Tokenizer
 from keras.utils import pad_sequences
@@ -10,42 +10,45 @@ from pickle import dump
 from sklearn.model_selection import train_test_split
 
 
-
-def define_model(in_vocab,out_vocab, in_seq_length,out_seq_length):
+def define_model(in_vocab, out_vocab, in_seq_length, out_seq_length):
     model = Sequential()
     model.add(Embedding(in_vocab, 50, input_length=in_seq_length))
     model.add(LSTM(100))
     model.add(RepeatVector(out_seq_length))
-    model.add(LSTM(100,return_sequences=True))
+    model.add(LSTM(100, return_sequences=True))
     model.add(Dense(100, activation='relu'))
     model.add(Dense(out_vocab, activation='softmax'))
     # compile network
-    model.compile(loss='sparse_categorical_crossentropy',optimizer='RMSprop')
+    model.compile(loss='sparse_categorical_crossentropy', optimizer='RMSprop')
     # summarize defined model
     model.summary()
     return model
+
 
 def tokenization(sequences):
     tokenizer = Tokenizer()
     tokenizer.fit_on_texts(sequences)
     return tokenizer
 
-def do_padding(sequences,length):
+
+def do_padding(sequences, length):
     new = []
     for i in sequences:
-        if len(i)>=length:
+        if len(i) >= length:
             new.append(i[:length])
         else:
             new.append(i+[0 for _ in range(length-len(i))])
     return new
 
-def encoding(sequences,tokenizer,length):
+
+def encoding(sequences, tokenizer, length):
     seq = tokenizer.texts_to_sequences(sequences)
     # seq = do_padding(seq, length)
-    seq = pad_sequences(seq,maxlen=length,padding="post")
+    seq = pad_sequences(seq, maxlen=length, padding="post")
     return seq
 
-def setup_model(source,target):
+
+def setup_model(source, target):
     en_tokenizer = tokenization(source)
     hu_tokenizer = tokenization(target)
     print("tokenizing")
@@ -54,39 +57,58 @@ def setup_model(source,target):
 
     en_seq_length = len(source[0])*2
     hu_seq_length = len(source[0])*2
-    
-    print(en_seq_length)
-    
-    input_sequences = encoding(source,en_tokenizer,en_seq_length)
-    output_sequences = encoding(target,hu_tokenizer,hu_seq_length)
-    print("encoding")
-    
-    return input_sequences,output_sequences,en_seq_length,hu_seq_length,\
-        en_tokenizer,hu_tokenizer,en_size,hu_size
 
-def training_model(input_seq,output_seq,model):
+    print(en_seq_length)
+
+    input_sequences = encoding(source, en_tokenizer, en_seq_length)
+    output_sequences = encoding(target, hu_tokenizer, hu_seq_length)
+    print("encoding")
+
+    return input_sequences, output_sequences, en_seq_length, hu_seq_length,\
+        en_tokenizer, hu_tokenizer, en_size, hu_size
+
+
+def training_model(input_seq, output_seq, model):
     model.fit(input_seq, output_seq, batch_size=128, epochs=10)
     model.save('../model.h5')
     return
+
 
 def splitting():
     df = pd.read_pickle('../clean-data.pkl')[:10000]
     print(len(df))
     x_train, x_test, y_train, y_test = train_test_split(df["source"], df["target"],
-                                                        test_size=0.2,random_state=42)
+                                                        test_size=0.2, random_state=42)
     return x_train, x_test, y_train, y_test
+
 
 def training_proces():
     source, source_test, target, target_test = splitting()
-    input_seq,output_seq,en_len,hu_len,en_tokenizer,hu_tokenizer,\
-        en_size,hu_size = setup_model(source.tolist(),target.tolist())
-    model = define_model(en_size,hu_size,en_len,hu_len)
+    input_seq, output_seq, en_len, hu_len, en_tokenizer, hu_tokenizer,\
+        en_size, hu_size = setup_model(source.tolist(), target.tolist())
+    # model = define_model(en_size, hu_size, en_len, hu_len)
+    print("INPUT")
+    print(input_seq)
+
+    for idx in input_seq[0]:
+        for word, transform in en_tokenizer.word_index.items():
+            if transform == idx:
+                print(word, " ", end='')
+
+    print("OUTPUT")
+    print(output_seq)
+
+    for idx in output_seq[0]:
+        for word, transform in hu_tokenizer.word_index.items():
+            if transform == idx:
+                print(word, " ", end='')
     print("model")
-    training_model(input_seq,output_seq,model)
-    print("training")
-    dump(en_tokenizer, open('en_tokenizer.pkl', 'wb'))
-    dump(hu_tokenizer, open('hu_tokenizer.pkl', 'wb'))
+    # training_model(input_seq,output_seq,model)
+    # print("training")
+    # dump(en_tokenizer, open('en_tokenizer.pkl', 'wb'))
+    # dump(hu_tokenizer, open('hu_tokenizer.pkl', 'wb'))
     return
+
 
 if __name__ == "__main__":
     training_proces()
